6529825317ad58240744eb1a4b082d6594d2b09f
kate <2951525@gmail.com>
2021-05-30 02:22:53 +0300

    Commit task2 and task3

--- app/src/main/java/com/example/katerinasapp/MainActivity.java
+++ app/src/main/java/com/example/katerinasapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.katerinasapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
     }
+
+
 }
